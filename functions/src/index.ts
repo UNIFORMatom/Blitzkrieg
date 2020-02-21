83679173cdecdd4dda541e3fac0dbf1104821c77
Aswin B <aswinbabu902@gamil.com>
2020-02-22 01:11:25 +0530

    Revert "Merge branch 'dev'"
    This reverts commit 54fd993873f10e9fc4a9671b99905ca12e1051c8, reversing
    changes made to 1f4f5117348e7151609b52dade6a7fa444913f7b.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -11,31 +11,52 @@ export const getUpdatedBalance = functions.storage.object().onFinalize(async (ob
   
     const fileBucket = object.bucket;
     const filePath:string = object.name!;
-    const contentType = object.contentType;
+    //const contentType = object.contentType;
     
+    const newMetadata = {
+        contentType: 'text/plain',
+    };
 
-    if (contentType?.startsWith('image/')){
-        console.log('not a text file');
-    }
-    //downloading file
+//downloading file
     const fileName = path.basename(filePath);
     const bucket = admin.storage().bucket(fileBucket);
     const tempFilePath = path.join(os.tmpdir(), fileName);
 
-    const metadata = {
-        contentType: contentType,
-      };
-
-    await bucket.file(filePath).download({destination: tempFilePath});
+    await bucket.file(filePath).download({destination: tempFilePath})
+    .then( downloaded => {
+        console.log("File successfully downloaded: ", fileName);
+    } )
+    .catch( dnfailed => {
+        console.log("file download failed: ", fileName);
+    })
     console.log('File downloaded locally to', tempFilePath);
 
-    const myNewFile = new File(fileName, 'tempname.txt', {type: 'txt'});
+//changing metadata in the servers local storage
+    fileName.updateMetadata(newMetadata).then( function(metadata: any) {
+        console.log('metadata changed: ', metadata)
+    })
+    .catch( function(error: any) {
+        console.log('error', error)
+    } )
 
-    const tempFilePath1 = path.join(path.dirname(filePath), myNewFile);
+    ///const myFile = fileName;
+    fs.readFile(fileName, (err: any, data: any) => {
+        if(err) throw err;
+        console.log(data.toString());
+    })
+
+//uploading the new file back to firebase storage
+    const tempFilePath1 = path.join(path.dirname(filePath), fileName);
     await bucket.upload(tempFilePath, {
         destination: tempFilePath1,
-        metadata: metadata,
-    });
+        metadata: newMetadata,
+    })
+    .then( uploaded => {
+        console.log("file successfully uploaded: ", fileName)
+    })
+    .catch( upfailed => {
+        console.log('file upload failed: ', fileName)
+    })
     return fs.unlinkSync(tempFilePath);
 
  });
