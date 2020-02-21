2e230d2c3ffe7269986384a9cbb56395c37d7095
Aswin B <aswinbabu902@gamil.com>
2020-02-22 01:26:33 +0530

    reverting back

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -13,50 +13,24 @@ export const getUpdatedBalance = functions.storage.object().onFinalize(async (ob
     const filePath:string = object.name!;
     //const contentType = object.contentType;
     
-    const newMetadata = {
+    const metadata = {
         contentType: 'text/plain',
     };
 
-//downloading file
+    //downloading file
     const fileName = path.basename(filePath);
     const bucket = admin.storage().bucket(fileBucket);
     const tempFilePath = path.join(os.tmpdir(), fileName);
 
-    await bucket.file(filePath).download({destination: tempFilePath})
-    .then( downloaded => {
-        console.log("File successfully downloaded: ", fileName);
-    } )
-    .catch( dnfailed => {
-        console.log("file download failed: ", fileName);
-    })
+    await bucket.file(filePath).download({destination: tempFilePath});
     console.log('File downloaded locally to', tempFilePath);
 
-//changing metadata in the servers local storage
-    fileName.updateMetadata(newMetadata).then( function(metadata: any) {
-        console.log('metadata changed: ', metadata)
-    })
-    .catch( function(error: any) {
-        console.log('error', error)
-    } )
-
-    ///const myFile = fileName;
-    fs.readFile(fileName, (err: any, data: any) => {
-        if(err) throw err;
-        console.log(data.toString());
-    })
-
-//uploading the new file back to firebase storage
+    //const myNewFile = 'new_${fileName}';
     const tempFilePath1 = path.join(path.dirname(filePath), fileName);
     await bucket.upload(tempFilePath, {
         destination: tempFilePath1,
-        metadata: newMetadata,
-    })
-    .then( uploaded => {
-        console.log("file successfully uploaded: ", fileName)
-    })
-    .catch( upfailed => {
-        console.log('file upload failed: ', fileName)
-    })
+        metadata: metadata,
+    });
     return fs.unlinkSync(tempFilePath);
 
  });
