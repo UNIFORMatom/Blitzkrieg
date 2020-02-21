b22fe593d5c14c74a23dfac1e5817831a5b1be79
Aswin B <aswinbabu902@gamil.com>
2020-02-21 22:51:22 +0530

    Firebase Function codes
    Deleted all the android studio files

--- README.md
+++
@@ -1,6 +0,0 @@
-# Blitzkrieg
-GitHub repository for Paypal Hackathon
-
-Paypal hackathon organized by Shastra University in Thanjavur.
-
-Team Members: Aswin Alwin Ivan Jagathnath
---
+++ functions/src/index.ts
@@ -0,0 +1,41 @@
+import * as functions from 'firebase-functions'
+import * as admin from 'firebase-admin'
+//import { object } from 'firebase-functions/lib/providers/storage';
+admin.initializeApp()
+//const spawn = require('child-process-promise').spawn;
+const path = require('path');
+const os = require('os');
+const fs = require('fs');
+
+export const getUpdatedBalance = functions.storage.object().onFinalize(async (object) => {
+  
+    const fileBucket = object.bucket;
+    const filePath:string = object.name!;
+    const contentType = object.contentType;
+    
+
+    if (contentType?.startsWith('image/')){
+        return console.log('not a text file');
+    }
+    //downloading file
+    const fileName = path.basename(filePath);
+    const bucket = admin.storage().bucket(fileBucket);
+    const tempFilePath = path.join(os.tmpdir(), fileName);
+
+    const metadata = {
+        contentType: contentType,
+      };
+
+    await bucket.file(filePath).download({destination: tempFilePath});
+    console.log('File downloaded locally to', tempFilePath);
+
+    const myNewFile = new File(fileName, 'tempname.txt', {type: 'txt'});
+
+    const tempFilePath1 = path.join(path.dirname(filePath), myNewFile);
+    await bucket.upload(tempFilePath, {
+        destination: tempFilePath1,
+        metadata: metadata,
+    });
+    return fs.unlinkSync(tempFilePath);
+
+ });
